7490bc1d93b574b5cabd912610c55aeec18dee88
Your Name <you@example.com>
2021-01-26 15:41:41 +0530

    fc

--- src/component/Sum.js
+++ src/component/Sum.js
@@ -1,53 +1,62 @@
 import React, { Component } from "react";
+import { unmountComponentAtNode } from "react-dom";
 
 class Sum extends Component {
     state = {
-        num1: "",
-        num2: "",
-        result: ""
+        num1: "1",
+        num2: "2",
+        result: "3"
     }
     handleNum1(e) {
         const num1 = e.target.value;
-        this.setstate(prevState => {
+        this.setState(prevState => {
             return {
                 num1: num1,
                 num2: prevState.num2,
-                result: prevState.resu
+                result: prevState.result
 
             }
+
         })
+
     }
     handleNum2(e) {
         const num2 = e.target.value;
-        this.setstate(prevState => {
+        this.setState(prevState => {
             return {
                 num2: num2,
                 num1: prevState.num1,
-                result: prevState.resu
+                result: prevState.result
 
             }
+
         })
+
     }
     handleClick() {
         const num1 = this.state.num1;
         const num2 = this.state.num2;
         const result = parseInt(num1) + parseInt(num2)
-        this.setstate({
-            result: result
+        this.setState({
+            'result':result
+    
         })
-
+            console.log(this.state.result)
     }
     render() {
         return (
-            <>
+
+            <div className="container">
                 <div>
-                    <input onChange={this.handleNum1.bind(this)} value={this.state.num1}>Num1</input>
-                    <input onChange={this.handleNum2.bind(this)} value={this.state.num2}>Num2</input>
+                    <input onChange={this.handleNum1.bind(this)} value={this.state.num1}></input>
+                    <input onChange={this.handleNum2.bind(this)} value={this.state.num2}></input>
                     <button onClick={this.handleClick.bind(this)}>Sum</button>
+                    <div>
+                        {this.state.re}
+                    </div>
                 </div>
+            </div >
 
-
-            </>
         )
     }
 }
